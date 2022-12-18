08b2b2a0e1d1320a166aca848884a000d981c9d5
Zachary Taylor <zacharytaylor@tutamail.com>
2022-12-18 13:16:26 -0600

    Final tweaks and clean up

--- components/slideshow/slideshow.js
+++ components/slideshow/slideshow.js
@@ -39,12 +39,16 @@ const Slideshow = ({ images = [], imageURLs }) => {
     }
     setFullScreenMode(false);
   };
+  const preventDefaultFunc = (event) => {
+    if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+      event.preventDefault();
+    }
+  };
   const handleScreenClick = (event) => {
     if (!slideshowRef.current.contains(event.target)) {
       setFullScreenMode(false);
     }
   };
-
   const handleSlideInput = (event) => {
     switch (event.key) {
       case 'Escape':
@@ -74,24 +78,42 @@ const Slideshow = ({ images = [], imageURLs }) => {
       <div
         className={`inspiration-slideshow ${fullScreenMode ? 'fullscreen' : ''}`}
         onClick={(event) => handleScreenClick(event)}
+        onKeyDown={preventDefaultFunc}
         onKeyUp={(event) => handleSlideInput(event)}
-        role={fullScreenMode ? 'application' : ''}
       >
-        <div className="slideshow-container" ref={slideshowRef} tabIndex="-1">
+        <button
+          aria-label="Close Full Screen mode"
+          className="btn-slideshow-close"
+          onClick={closeFullScreen}
+          ref={btnCloseRef}
+        >
+          <span aria-hidden="true" className="icon"></span>
+        </button>
+        <div
+          aria-live="polite"
+          aria-roledescription="Image Slideshow"
+          className="slideshow-container"
+          ref={slideshowRef}
+          role={fullScreenMode ? 'application' : 'region'}
+          tabIndex="-1"
+        >
           {images.map((image, index) => {
             const imageUrl = imageURLs ? LoadedImageUrl(imageURLs, image.src) : image.src;
             return (
-              <div className={`slide fade ${currentSlideIndex === index ? 'active' : ''}`} key={index}>
-                <div className="numbertext">
+              <figure
+                aria-describedby={`count-${index}`}
+                aria-labelledby={`img-${index} caption-${index}`}
+                className={`slide fade ${currentSlideIndex === index ? 'active' : ''}`}
+                key={index}
+              >
+                <p id={`count-${index}`} className="numbertext">
                   {index + 1} / {images.length}
-                </div>
-                <figure>
-                  <img src={imageUrl} alt={image.alt} style={{ width: '100%' }} />
-                  <figcaption className="text" role="status" aria-relevant="text additions">
-                    {image.caption}
-                  </figcaption>
-                </figure>
-              </div>
+                </p>
+                <img src={imageUrl} alt={image.alt} style={{ width: '100%' }} id={`img-${index}`} />
+                <figcaption className="text" role="status" aria-relevant="text additions" id={`caption-${index}`}>
+                  {image.caption}
+                </figcaption>
+              </figure>
             );
           })}
 
